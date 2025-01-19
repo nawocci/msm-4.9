f294241989b7def650787b24801e75adf73218e1
Naufal Altaf <me@altaf.xyz>
2025-01-19 17:18:11 +0700

    Revert "OplusKernelEnvConfig: Disable macro set warning"
    This reverts commit 600d5eacc156ccc453b096300b76fcb2bb693dae.

--- OplusKernelEnvConfig.mk
+++ OplusKernelEnvConfig.mk
@@ -78,6 +78,7 @@ OPLUS_FEATURE_DUMPDEVICE
 
 
 $(foreach myfeature,$(ALLOWED_MCROS),\
+         $(warning make $(myfeature) to be a macro here) \
          $(eval KBUILD_CFLAGS += -D$(myfeature)) \
          $(eval KBUILD_CPPFLAGS += -D$(myfeature)) \
          $(eval CFLAGS_KERNEL += -D$(myfeature)) \
